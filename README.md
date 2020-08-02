4cc302c0eea56813e3fdb90c1454d503d5815fc8
Malcolm Nixon <malcolm.nixon@gmail.com>
2020-08-01 22:07:27 -0400

    Initial revision

---
+++ README.md
@@ -0,0 +1,21 @@
+# VHDL Testbench Runner
+This python module runs VHDL testbenches and generates a report of the results.
+
+It requires a VHDL Simulator be installed on the system. Supported simulators are:
+- [GHDL](http://ghdl.free.fr/)
+- [Aldec Active-HDL](https://www.aldec.com/en/products/fpga_simulation/active-hdl)
+
+# Installation
+VHDL Testbench Runner can be installed by running:
+```
+python -m pip install VHDLTest
+```
+
+# Configuring
+VHDL Testbench Runner requires a yaml configuration file to specify the project.
+
+# Running
+VHDL Testbench Runner can be run by;
+```
+python -m VHDLTest -c config.yaml
+```
---
+++ VHDLTest/__main__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import yaml
+
+def main():
+    """VHDL Testbench Runner application entry point"""
+    
+    # Construct the argument parser
+    parser = argparse.ArgumentParser(
+        prog='VHDL Testbench Runner (VHDLTest)',
+        description='''Runs VHDL Testbenches and generates a report of the
+                     passes and failures. Reference documentation is located
+                     at https://github.com/Malcolmnixon/VhdlTest''')
+    parser.add_argument('-c', '--config', help='Configuration file')
+    parser.add_argument('-v', '--version', default=False, action='store_true', help='Display version information')
+
+    # If no arguments are provided then print the help information
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+        
+    # Parse the arguments
+    args = parser.parse_args()
+                     
+    # Check for version
+    if args.version:
+        print('VHDL Testbench Runner (VHDLTest) version 0.0.1')
+        sys.exit(0)
+        
+    # Ensure we have a configuration
+    if args.config == None:
+        parser.print_help()
+        sys.exit(1)
+
+    
+if __name__ == '__main__':
+    main()
